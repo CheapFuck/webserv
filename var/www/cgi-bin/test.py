380fed3ec7720909d1ab07b69901c4312f7ab7d2
CheapFuck <root@DESKTOP-H11IBAH.localdomain>
2025-06-16 12:29:51 +0200

    lala

--- var/www/cgi-bin/test.py
+++ var/www/cgi-bin/test.py
@@ -1,11 +1,8 @@
 import time
-import sys
 
-try:
-    print("Hello world from Python CGI script!")
-    sys.stdout.flush()
-    time.sleep(10)
-    print("This is a test CGI script.")
-    sys.stdout.flush()
-except BrokenPipeError:
-    pass  # Prevent error on server if client disconnects
+output = []
+output.append("Hello world from Python CGI script!")
+time.sleep(10)
+output.append("This is a test CGI script.")
+
+print("\n".join(output))
