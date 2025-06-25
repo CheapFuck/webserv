213f7becf22fed45929b4b9500b4191e9813e5c8
Jose <jleonardola@gmail.com>
2025-06-25 15:10:27 +0200

    Fancier output and wait on input from user to start test

--- webtester/main.py
+++ webtester/main.py
@@ -3,13 +3,11 @@ from run_tests import run_tests
 import time
 
 def main():
-	print("\n\n ======= TESTING WEBSERV \nIf a test fails, the console will output assertion errors and stop there.")
-
-	try:
-		browser = get_browser()
-		run_tests(browser)
-	finally:
-		browser.quit()
+	print("\n\n ======= TESTING WEBSERV =======\nIf a test fails, the console will output assertion errors and stop there, and the browser will remain open.\n")
+	input("Press enter to start testing.")
+	browser = get_browser()
+	run_tests(browser)
+	browser.quit()
 
 if __name__ == "__main__":
 	main()
