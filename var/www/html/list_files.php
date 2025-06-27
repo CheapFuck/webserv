e95ef4bdcd4d14615c45e7fcbeffcd7d87d2b971
Thijs Van Den Berg <thivan-d@f1r4s1.codam.nl>
2025-06-27 16:05:31 +0200

    list

---
+++ var/www/html/list_files.php
@@ -0,0 +1,15 @@
+#!/usr/bin/php-cgi
+<?php
+$directory = '/../uploads/';
+
+if (!is_dir($directory)) {
+    http_response_code(500);
+    echo $directory . PHP_EOL;
+    echo json_encode(['error' => 'Directory not found']);
+    exit;
+}
+header("Status: 200");
+
+$files = array_diff(scandir($directory), array('..', '.'));
+echo json_encode(array_values($files));
+?>
