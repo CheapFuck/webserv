331b1775018c288b3f9de33f231029da7f548a3a
Jose <jleonardola@gmail.com>
2025-07-21 19:35:48 +0200

    Delete old configs folder

--- config/arena.cpp
+++
@@ -1,9 +0,0 @@
-#include "arena.hpp"
-
-Arena::~Arena() {
-    for (auto it = destructibles.rbegin(); it != destructibles.rend(); ++it)
-        it->destroy(it->ptr);
-    
-    for (void *block : memoryBlocks)
-        ::operator delete(block);
-}
