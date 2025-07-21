7071851bee59ab8861357879ec896c96a1d911ed
Jose <jleonardola@gmail.com>
2025-07-21 20:40:14 +0200

    Add a fodler subjectTester to deal with ubuntu_testg

---
+++ subjectTester/makeyoupi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -rf YoupiBanane
+mkdir -p YoupiBanane/nop
+mkdir -p YoupiBanane/Yeah
+
+touch YoupiBanane/youpi.bad_extension
+touch YoupiBanane/youpi.bla
+echo -e '#!/bin/bash\necho "hello"' > YoupiBanane/youpi.bla
+chmod +x YoupiBanane/youpi.bla
+
+touch YoupiBanane/nop/youpi.bad_extension
+touch YoupiBanane/nop/other.pouic
+
+touch YoupiBanane/Yeah/not_happy.bad_extension
